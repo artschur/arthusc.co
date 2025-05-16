8e5a84f30a9d9309206955ce4c8475edf0301df8
Arthur Schurhaus <arthurschur@icloud.com>
2025-05-16 19:01:54 -0300

    added mode switching

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import localFont from "next/font/local";
-import Balancer from 'react-wrap-balancer'
+import Balancer from "react-wrap-balancer";
 import Link from "next/link";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import ProjectPreview from "@/components/projectPreview";
-
+import { ModeToggle } from "@/components/theme-toggle";
 
 const redaction35Italic = localFont({
   src: "../public/font/Redaction35-Italic.otf",
@@ -30,30 +30,66 @@ const redaction = localFont({ src: "../public/font/Redaction-Regular.otf" });
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen py-6 text-base">
+    <main className="flex dark:bg-zinc-900 dark:text-white bg-zinc-50 text-zinc-950 flex-col items-center justify-center p-12 min-h-screen text-base md:py-6">
       <div className={`flex flex-col gap-y-8`}>
+        <ModeToggle />
         <div className={`${redaction35.className} text-4xl`}>
-          <Balancer>welcome to my portfolio,
+          <Balancer>
+            welcome to my portfolio,
             <br />
-            where i show my <RoughNotation type="underline" show={true} color="#f87171" padding={-5}>web things</RoughNotation>
-
+            where i show my{" "}
+            <RoughNotation type="underline" show={true} color="#f87171" padding={-5}>
+              web things
+            </RoughNotation>
           </Balancer>
-          <p className={`text-base ${redaction20Italic.className}`}>arthur is working on  <Link href={'/about'} className="underline decoration-gray-600">recreads</Link>...</p>
+          <p className={`text-base ${redaction20Italic.className}`}>
+            arthur is working on{" "}
+            <Link href={"/about"} className="underline dark:decoration-gray-600">
+              recreads
+            </Link>
+            ...
+          </p>
         </div>
         <div className="text-base max-w-2xl ">
-          my name is <span className="underline decoration-dotted decoration-zinc-200">arthur schurhaus</span>, and the tools im experienced in are: typescript, nextjs, php and sql - and i focus on <RoughNotation type="highlight" show={true} color="#f87171" animationDelay={1500}>user-experience and performance</RoughNotation><br /> <br />
-          and when i'm not coding, you'll find me producing music, reading some fiction or diving deep into theories about my favorite tv shows.
-
-          <br />
-          i am a student of  <span className="underline decoration-zinc-200"> information systems</span> at the federal university of santa catarina, securing 3rd place in the university's startup competition and <span className="underline decoration-red-400"> winning</span> startup weekend floripa by creating a <Link href={'cybersure'} className="font-mono">cybersec</Link> startup 👨🏻‍💻.
+          my name is{" "}
+          <span className="underline decoration-dotted dark:decoration-zinc-800 italic">
+            arthur schurhaus
+          </span>
+          , and my weapons of choice are: golang, typescript and nextjs - and i focus on{" "}
+          <RoughNotation
+            type="highlight"
+            multiline={true}
+            show={true}
+            color="#f87171"
+            animationDelay={1500}
+          >
+            user-experience and performance
+          </RoughNotation>
+          <br /> <br />
+          and when i'm not coding, you'll find me producing music or reading some good books.
+          <br />i am a student of{" "}
+          <span className="underline dark:decoration-zinc-200"> information systems</span> at the
+          federal university of santa catarina, and i work as a software engineer at a company
+          called costao do santinho, where i build several, performant golang microservices and API's.
         </div>
 
         <section className="">
+          <h3 className={`${redaction20Italic.className}`}>Let me introduce myself 💫</h3>
           <ProjectPreview projectName="recreads" date="2025" link="recreads" field="fullstack" />
-          <ProjectPreview projectName="revisacard" date="2024" link="revisacard" field="landing page" />
-          <ProjectPreview projectName="esse é um projeto com um titulo maior sobre hahah" date="2024" link="about" field="landing page" />
+          <ProjectPreview
+            projectName="revisacard"
+            date="2024"
+            link="revisacard"
+            field="landing page"
+          />
+          <ProjectPreview
+            projectName="esse é um projeto com um titulo maior sobre hahah"
+            date="2024"
+            link="about"
+            field="landing page"
+          />
         </section>
       </div>
-    </main >
+    </main>
   );
 }
--- components/projectPreview.tsx
+++ components/projectPreview.tsx
@@ -1,22 +1,34 @@
 import Link from "next/link";
 
-export default function ProjectPreview({ projectName, date, link, field }: { projectName: string, date: string, link: string, field: string }) {
-    return (
-        <div className="flex flex-col items-start gap-y-4">
-            <div className="flex flex-col items-start">
-                <Link
-                    href={`${link}`}
-                    className="hover:bg-neutral-100 py-2 px-4 -ml-4 rounded-md
+export default function ProjectPreview({
+  projectName,
+  date,
+  link,
+  field,
+}: {
+  projectName: string;
+  date: string;
+  link: string;
+  field: string;
+}) {
+  return (
+    <div className="flex flex-col items-start gap-y-4">
+      <div className="flex flex-col items-start">
+        <Link
+          href={`${link}`}
+          className="dark:hover:bg-zinc-800 hover:bg-zinc-100  py-2 px-4 -ml-4 rounded-md
                     transition-all duration-300 ease-in-out
                     hover:scale-[1.02] hover:shadow-sm"
-                >
-                    <h1 className="text-normal underline underline-offset-2 decoration-zinc-100">{projectName}</h1>
-                    <div className="flex flex-row justify-between align-bottom gap-x-12 w-full">
-                        <p className="text-sm text-zinc-400 ">{date}</p>
-                        <p className="text-xs text-zinc-400 font-mono">{field}</p>
-                    </div>
-                </Link>
-            </div>
-        </div>
-    );
+        >
+          <h1 className="text-normal underline underline-offset-2 decoration-zinc-200 dark:decoration-zinc-800">
+            {projectName}
+          </h1>
+          <div className="flex flex-row justify-between align-bottom gap-x-12 w-full">
+            <p className="text-sm text-zinc-800 dark:text-zinc-300 ">{date}</p>
+            <p className="text-xs text-zinc-800 dark:text-zinc-300 font-mono">{field}</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
